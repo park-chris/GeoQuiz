6e52ee060c3d643b00bfa1defe123007f472bd9b
박수정 <zxzx4342@naver.com>
2022-01-24 15:32:34 +0900

    ">" 아이콘 추가 + NEXT 버튼 기능 추가

--- app/src/main/java/com/crystal/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/crystal/android/geoquiz/MainActivity.kt
@@ -5,33 +5,74 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private var questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true)
+    )
+
+    private var currentIndex = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
 
-            var testToast: Toast =Toast.makeText(this,R.string.correct_toast, Toast.LENGTH_SHORT)
-            testToast.setGravity(Gravity.TOP, 200, 200 )
-            testToast.show()
+            checkAnswer(true)
 
         }
 
         falseButton.setOnClickListener { view: View ->
 
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
 
         }
 
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+
+    }
+
+//    현재 인덱스에 따라 문제질문을 업데이트해주는 함수
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+//    리스트에 있는 답과 사용자가 찍은 답이 일치하는지 체크하는 함수
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
